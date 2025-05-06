eefa0a33bea3ec9f3479a014091b72aebb793d11
Reynaldo Garcia <garciar17@southernct.edu>
2025-05-05 20:50:51 -0400

    Altered file

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-
+    
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True, nullable=False)
